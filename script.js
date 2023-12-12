a76dcdc2c456ca4490297e6e11187f2e642a68fb
fegy0 <137124051+fegy0@users.noreply.github.com>
2023-12-12 20:05:08 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var descargarBoton = document.getElementById("descargarBoton");
+    var codigoQR = document.getElementById("codigoQR");
+
+    descargarBoton.addEventListener("click", function (){
+        var userAgent = navigator.userAgent.toLowerCase();
+
+        codigoQR.innerHTML = "";
+
+        if(userAgent.match(/android|webos|iphone|ipad|ipod|blackberry|windows phone/i))
+        {
+            window.location.href = "APK/app-debug.apk";
+        } else {
+            codigoQR.style.display = "block";
+            
+            var qr = new QRCode(document.getElementById("codigoQR"), {
+                text: "https://github.com/fegy0/fegy0.github.io/blob/b0db38e167d17b7418dda896e74ccd62a0dfb494/app-debug.apk",
+                width: 128,
+                height: 128
+            })
+        }
+    });
+});
